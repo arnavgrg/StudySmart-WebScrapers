f4b247aa5cc1370c2de2dd02b4caebb5a464d9a8
Arnav Garg <arnavgrg@gmail.com>
2019-01-25 10:31:08 -0800

    retrieve html text

--- StudySmartPowellScraper/StudySmartPowellScraper.py
+++ StudySmartPowellScraper/StudySmartPowellScraper.py
@@ -9,5 +9,10 @@ if __name__ == '__main__':
     #Check if request returns 200
     if r.ok:
         soup = bs(r.text, "html.parser")
+        #print(soup.prettify())
+        #for item in soup.body.a.findall(attrs={'class': 'fc-timeline-event'}):
+         #   print(item['title'])
+        #for item in soup.findall(attrs={'class' : 'fc-timeline-event'}):
+         #   print(item.a['title'])
     else:
         sys.stderr.write("Link returns bad signal\n")
