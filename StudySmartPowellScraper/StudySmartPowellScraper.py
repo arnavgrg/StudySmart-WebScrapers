501d1c5e34ca5d01cf3ff50deb4a7bea43746397
Arnav Garg <arnavgrg@gmail.com>
2019-01-25 10:06:54 -0800

    retrieve text

--- StudySmartPowellScraper/StudySmartPowellScraper.py
+++ StudySmartPowellScraper/StudySmartPowellScraper.py
@@ -1,9 +1,13 @@
 import json
 from bs4 import BeautifulSoup as bs
 import requests
-
-
+import sys
 
 if __name__ == '__main__':
-    print("hi")
-
+    url = "http://calendar.library.ucla.edu/spaces?lid=4394&gid=7748"
+    r = requests.get(url)
+    #Check if request returns 200
+    if r.ok:
+        soup = bs(r.text, "html.parser")
+    else:
+        sys.stderr.write("Link returns bad signal\n")
